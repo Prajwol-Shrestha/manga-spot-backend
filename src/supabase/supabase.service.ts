b8a842daab885802782810f04ff35755ae7c57e8
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2025-08-05 19:57:04 +0545

    chore: add supabase type:

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from 'src/types/supabase';
 
 @Injectable()
 export class SupabaseService {
-    private supabase = createClient(
+    private supabase = createClient<Database>(
         process.env.SUPABASE_URL!,
         process.env.SUPABASE_SERVICE_ROLE_KEY!
     )
